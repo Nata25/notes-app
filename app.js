8a8f01a6474dadbf98b140cbcbcb897a46620bf0
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-03-13 18:26:03 +0200

    Working with commands

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 console.log('Starting app.js...');
 const fs = require('fs');
-const os = require('os');
+
 const _ = require('lodash');
 const notes = require('./note.js');
 
-// let result = notes.addNote();
-// let result = notes.add(3, 4);
-let result = _.isString('abc');
-console.log(result);
-result = _.isString(111);
-console.log(result);
-result = _.uniq(['Natalya', '111', '222', '111', true, true, false]);
-console.log(result);
+const command = process.argv[2];
+console.log(command);
+console.log(process.argv);
 
-// const user = os.userInfo();
-// console.log(user);
-
-// fs.appendFile('greetings.txt', 'hello', function(err) {
-    // if (err) console.log('Unable to write to file');
-// });
-
-// fs.appendFileSync('greetings.txt', `Hello, ${user.username}! You are ${notes.age}.`);
+if (command === 'add') console.log('Add a note');
+else if (command === 'list') console.log('Printing notes');
+else if (command === 'read') console.log('Fetching note...');
+else if (command === 'remove') console.log('Removing note...');
+else console.log('Command is unrecognized');
